26d34fc31017533f133a79c6688bdbe7a9a43d7c
howard1414 <lv3790620@gmail.com>
2021-04-21 14:32:01 +0800

    Initial Commit

---
+++ Calculator midtern/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Calculator midtern
+//
+//  Created by Lab1422 on 2021/4/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
